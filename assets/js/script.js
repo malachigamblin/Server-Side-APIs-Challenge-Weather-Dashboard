4f3e46b965d9759529e98d277c3e2cf1e9616fab
malachigamblin <malachi.gamblin@gmail.com>
2023-03-02 14:25:21 -0500

    set up function to call openweatherapi

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var APIkey = "f9f8fe997d95f05921c4379ec1099394";
+
+var currentCity;
+var cityInputEl = $('#city-input');
+var searchBtn = $('#search-button');
+var clearBtn = $('#clear-button');
+var pastSearchedCitiesEl = $('#past-searches');
+
+function getWeather(data) {
+    var requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
+    fetch(requestURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            
+        })
+
+}
+
